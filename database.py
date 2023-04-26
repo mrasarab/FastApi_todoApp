c12cf9546cda58fae2fb6ee1b1af6e8199af8ad0
mrasarab <120579485+mrasarab@users.noreply.github.com>
2023-04-27 00:09:24 +0200

    Update database.py

--- database.py
+++ database.py
@@ -6,12 +6,12 @@ client = MongoClient(
 database = client.todo_application
 collection = database.todos
 
-
+# Fetch a single todo by title
 async def fetch_one_todo(title):
     document = collection.find_one({"title": title})
     return document
 
-
+# Fetch all todos in the collection
 def fetch_all_todos():
     all_todos = []
     cursor = collection.find({})
@@ -19,21 +19,19 @@ def fetch_all_todos():
         all_todos.append(todo(**document))
     return all_todos
 
-
+# Create a new todo in the collection
 def create_todo(todo):
     document = todo
     result = collection.insert_one(document)
     return document
 
-
+# Update the description of a todo by title
 async def update_todo(title, description):
     await collection.update_one({"title": title}, {"$set": {"description": description}})
     document = collection.find_one({"title": title})
     return document
 
-
+# Remove a todo from the collection by title
 async def remove_todo(title):
     await collection.delete_one({"title": title})
     return True
-
-
